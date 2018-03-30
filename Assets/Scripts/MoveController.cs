8c02ba135ef5ec33e8a878d399ee465d148b534e
coreylee1989 <coreylee1989@gmail.com>
2018-03-30 09:40:52 -0500

    Initial Commit

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+
+	public float speed;
+	private Rigidbody2D rb;
+	public GameObject laserHit;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2 (-speed, rb.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Collision");
+		Instantiate(laserHit, other.transform.position, other.transform.rotation);
+		Destroy (other.gameObject);
+	}
+		
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private Animator anim;
+
+	public GameObject laserBullet;
+	public Transform firePoint;
+
+	Vector2 moveInput;
+
+	public float speed;
+	public Transform topLeft;
+	public Transform bottomRight;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		rb.velocity = moveInput * speed;
+
+		anim.SetFloat ("Movement", Input.GetAxisRaw ("Vertical"));
+
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, topLeft.position.x, bottomRight.position.x),
+			Mathf.Clamp(transform.position.y, bottomRight.position.y, topLeft.position.y), transform.position.z);
+		if (Input.GetButtonDown ("Fire1")) {
+			Debug.Log ("Fire laser");
+			Instantiate (laserBullet, firePoint.position, firePoint.rotation); 
+		}	
+
+	}
+}
